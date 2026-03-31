3575b94c7c30d7e220f8be578db1a571976a2143
loren-gc <lorenzo.chiachio@estudante.ufscar.br>
2026-03-31 00:43:53 -0300

    Adding list and pointer tests

--- struct_test.cs
+++ struct_test.cs
@@ -1,5 +1,7 @@
 
 // STRUCT TEST
+using System.Runtime.CompilerServices;
+
 Student teste = new Student("Ralph");
 int variable = teste.GetGrade();
 Console.WriteLine($"test1 = {variable}");
@@ -26,6 +28,27 @@ Person amanda = new Person("Amanda");
 Console.WriteLine($"previous age = {amanda.getAge()}");
 amanda.putAge(22);
 Console.WriteLine($"amanda age = {amanda.getAge()}");
+
+// VECTOR AND POINTER TEST
+List<int> list = new List<int>();
+for (int i=0; i<10; i++) list.Add(0);
+int[] vector = list.ToArray();
+
+for (int i=0; i<10; i++) Console.Write($"{vector[i]} ");
+Console.Write("\n");
+Console.Write("\n");
+
+unsafe
+{
+    fixed (int* vPointer = &vector[0])
+    {
+        *vPointer = 10;
+    }
+
+}
+
+for (int i=0; i<10; i++) Console.Write($"{vector[i]} ");
+Console.Write("\n");
 
 //=============================================== STRUCT AND CLASS =======================================================
 struct Student
