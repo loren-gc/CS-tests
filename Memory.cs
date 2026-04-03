23671a3a6f06b72c76ad0bd44651402a82673cde
loren-gc <lorenzo.chiachio@estudante.ufscar.br>
2026-04-03 03:33:22 -0300

    Adding pointer deallocation

--- Memory.cs
+++ Memory.cs
@@ -12,14 +12,17 @@ public class Memory
 	
 	public void vectorCreation()
 	{
+		int size = this.memoryQtd*sizeof(int);
 		unsafe
 		{
-			int size = this.memoryQtd*sizeof(int);
 			IntPtr p = Marshal.AllocHGlobal(size);
 			int* pointer = (int*)p.ToPointer();
+			
+			Console.Write("Vector creation method = ");
 			for (int i=0; i<this.memoryQtd; i++) pointer[i] = i;
 			for (int i=0; i<this.memoryQtd; i++) Console.Write($"{pointer[i] }");
 			Console.Write("\n");
+			Marshal.FreeHGlobal(p);
 		}
 	}
 	
