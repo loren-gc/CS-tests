4b869b560b12566a4c2b60c0715bbf90a0faf918
loren-gc <lorenzo.chiachio@estudante.ufscar.br>
2026-04-28 09:06:53 -0300

    Testing reading file commands

--- tests2/Program.cs
+++ tests2/Program.cs
@@ -1,7 +1,21 @@
-﻿class Program
+﻿using System;
+using System.IO;
+using System.Net.Mime;
+using System.Text;
+
+class Program
 {
     static void Main()
     {
-        Console.WriteLine("hello?");
+        
+        using (FileStream file = File.Open("test_file.txt", FileMode.Open))
+        {
+            byte[] file_buffer = new byte[file.Length];
+            int read_bytes = file.Read(file_buffer, 0, file_buffer.Length);
+            string content = Encoding.UTF8.GetString(file_buffer);
+            Console.WriteLine($"This file has {read_bytes} bytes\n");
+            Console.Write(content);
+        }
+
     }
 }
